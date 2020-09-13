26c7a6edd9c54e655309c98829e20322d676f114
OrShemesh <orshemesh5020@gmail.com>
2020-09-13 22:10:38 +0300

    init commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+app.listen(4000, () => {
+  console.log("server is up: ", 4000);
+});
+
+app.get("/status", (req, res)=>{
+    res.sendStatus(200);
+})
