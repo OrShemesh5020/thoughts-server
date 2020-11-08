4bd9b1e5e33d2af2297f01482fab20039c7d00b6
Eliran Shemesh <eliran.shemesh@netomedia.com>
2020-11-08 18:05:06 +0200

    format server.js code

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 app.listen(4000, () => {
-  console.log("server is up: ", 4000);
+  console.log('server is up: ', 4000);
 });
 
-app.get("/status", (req, res)=>{
-    res.sendStatus(200);
+app.get('/status', (req, res) => {
+  res.sendStatus(200);
 });
